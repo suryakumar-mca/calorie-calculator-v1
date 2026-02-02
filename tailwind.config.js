f1328f98adf9835f680767acceac90a222b5cf8a
SURYAKUMAR ARUMUGAM <suryakumar@SURYAKUMARs-MacBook-Air.local>
2026-02-02 20:02:34 +0530

    Added tailwind locally

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./app.py"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
